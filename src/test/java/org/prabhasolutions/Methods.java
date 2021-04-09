c920647188e79d8f97b14b42727677c2cbf4d0a3
prabhakaran6015 <prabakar.peter91@gmail.com>
2021-04-09 13:52:39 +0530

    Git

---
+++ src/test/java/org/prabhasolutions/Methods.java
@@ -0,0 +1,31 @@
+package org.prabhasolutions;
+
+public class Methods {
+	
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+	public void method3()
+	{
+		System.out.println("method3");
+	}
+	public void method4()
+	{
+		System.out.println("method4");
+	}
+
+	public static void main(String[] args) {
+		Methods m = new Methods();
+		m.method1();
+		m.method2();
+		m.method3();
+		m.method4();
+
+	}
+
+}
